e3e38fa32ac56ff00607cf715a0ac29dea0e3e36
Paolo Lami <paololami2000@gmail.com>
2021-11-17 21:15:21 +0000

    test

--- MotorTest.py
+++ MotorTest.py
@@ -29,11 +29,11 @@ try:
 
   GPIO.output(Motor1cw,GPIO.HIGH)
   GPIO.output(Motor1ccw,GPIO.LOW) #turn HIGH for counterclockwise
-  GPIO.output(pwm1,GPIO.HIGH)
+  GPIO.output(pwmPin1,GPIO.HIGH)
 
   GPIO.output(Motor2cw,GPIO.LOW)
   GPIO.output(Motor2ccw,GPIO.HIGH) #turn HIGH for counterclockwise
-  GPIO.output(pwm2,GPIO.HIGH)
+  GPIO.output(pwmPin2,GPIO.HIGH)
 
 except KeyboardInterrupt:
   print("Ending")
