8ae8b450ec9e9511ebd98a8d45108a4bf1b26742
Paolo Lami <paololami2000@gmail.com>
2021-11-22 22:47:08 +0000

    dir

--- MotorTest.py
+++ MotorTest.py
@@ -44,8 +44,8 @@ try:
     #pwm1.ChangeDutyCycle(dc1)
     #M1ccw.ChangeDutyCycle(dc1)
 
-    GPIO.output(Motor2cw,GPIO.HIGH)
-    GPIO.output(Motor2ccw,GPIO.LOW) #turn HIGH for counterclockwise
+    GPIO.output(Motor2cw,GPIO.LOW)
+    GPIO.output(Motor2ccw,GPIO.HIGH) #turn HIGH for counterclockwise
     #GPIO.output(Motor2EN,GPIO.HIGH)
     #speed2 = input("Set speed for motor 2: ")
     #dc2=int(speed2)
