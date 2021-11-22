98bdaceace14a30c9828ca58f4de7f15fde7411b
Paolo Lami <paololami2000@gmail.com>
2021-11-22 20:48:26 +0000

    pwm

--- MotorTest.py
+++ MotorTest.py
@@ -7,9 +7,9 @@ Motor1cw = 23 # Motor 1 +Red pin
 Motor1ccw = 24 # Motor 1 +Ground pin
 Motor2cw = 5 # Motor 2 +Red pin
 Motor2ccw = 6 # Motor 2 +Ground pin
-pwmPin1 = 25 #Enabler for motor 1
-pwmPin2 = 26 #Enabler for motor 2
-#Plug both Vcc to Pi 3.3V
+Motor1EN = 25 #Enabler for motor 1
+Motor2EN = 26 #Enabler for motor 2
+#Plug both Vcc to Pi 5V
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Motor1cw,GPIO.OUT) 
@@ -17,31 +17,59 @@ GPIO.setup(Motor1ccw,GPIO.OUT)
 GPIO.setup(Motor2cw,GPIO.OUT)
 GPIO.setup(Motor2ccw,GPIO.OUT)
 
-GPIO.setup(pwmPin1, GPIO.OUT)
-GPIO.setup(pwmPin2, GPIO.OUT)
+GPIO.setup(Motor1EN, GPIO.OUT)
+GPIO.setup(Motor2EN, GPIO.OUT)
 
-#pwm1=GPIO.PWM(pwmPin1,100)
-#pwm2=GPIO.PWM(pwmPin2,100)
+#PWM setup
+M1cw=GPIO.PWM(Motor1EN,100)
+M1ccw=GPIO.PWM(Motor2EN,100)
+M2cw=GPIO.PWM(Motor1EN,100)
+M2ccw=GPIO.PWM(Motor2EN,100)
+pwm1=GPIO.PWM(Motor1EN,100)
+pwm2=GPIO.PWM(Motor2EN,100)
+pwm1.start(0)
+pwm2.start(0)
+M1cw.start(0)
+M1ccw.start(0)
+M2cw.start(0)
+M2ccw.start(0)
 
 try:
-  while True:
-    #pwm1.start(100)
-    #pwm2.start(100)
 
-    GPIO.output(Motor1cw,GPIO.LOW)
-    GPIO.output(Motor1ccw,GPIO.HIGH) #turn HIGH for counterclockwise
-    GPIO.output(pwmPin1,GPIO.HIGH)
+    #GPIO.output(Motor1cw,GPIO.LOW)
+    #GPIO.output(Motor1ccw,GPIO.HIGH) #turn HIGH for counterclockwise
+    #GPIO.output(Motor1EN,GPIO.HIGH)
+    pwm1.setDutyCycle(99)
+    M1ccw.setDutyCycle(99)
 
-    GPIO.output(Motor2cw,GPIO.HIGH)
-    GPIO.output(Motor2ccw,GPIO.LOW) #turn HIGH for counterclockwise
-    GPIO.output(pwmPin2,GPIO.HIGH)
+    #GPIO.output(Motor2cw,GPIO.HIGH)
+    #GPIO.output(Motor2ccw,GPIO.LOW) #turn HIGH for counterclockwise
+    #GPIO.output(Motor2EN,GPIO.HIGH)
+    pwm2.setDutyCycle(99)
+    M2cw.setDutyCycle(99)
 
-    time.sleep(60)    
+    time.sleep(300)
+
+    #GPIO.output(Motor1EN,GPIO.LOW)
+    #GPIO.output(Motor2EN,GPIO.LOW)
+    pwm1.stop(0)
+    pwm2.stop(0)
+    M1cw.stop(0)
+    M1ccw.stop(0)
+    M2cw.stop(0)
+    M2ccw.stop(0) 
+
+    GPIO.cleanup()   
+
 except KeyboardInterrupt:
   print("Ending")
-  GPIO.output(pwmPin1,GPIO.LOW)
-  GPIO.output(pwmPin2,GPIO.LOW)
-  #pwm1.stop()
-  #pwm2.stop()
-GPIO.cleanup() 
+  #GPIO.output(Motor1EN,GPIO.LOW)
+  #GPIO.output(Motor2EN,GPIO.LOW)
+  pwm1.stop(0)
+  pwm2.stop(0)
+  M1cw.stop(0)
+  M1ccw.stop(0)
+  M2cw.stop(0)
+  M2ccw.stop(0)
+  GPIO.cleanup() 
 
