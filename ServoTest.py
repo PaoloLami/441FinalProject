6d8c2e742f54a1946a6731d7eb70427893d5c255
Paolo Lami <paololami2000@gmail.com>
2021-11-29 21:07:01 +0000

    test

--- ServoTest.py
+++ ServoTest.py
@@ -18,10 +18,13 @@ pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
 try:
   while True:
-    for dc in range(dcMin,dcMax):
-      pwm.ChangeDutyCycle(dc)
-      print(dc)
-      time.sleep(0.1)
+    #for dc in range(dcMin,dcMax):
+    pwm.ChangeDutyCycle(dcMin)
+      #print(dc)
+    time.sleep(0.1)
+    pwm.ChangeDutyCycle(dcMax)
+    time.sleep(0.1)
+
 except KeyboardInterrupt:
   print("bye")
 GPIO.cleanup() 
