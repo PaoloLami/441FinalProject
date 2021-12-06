00e64704a39066217f57639514f1ea03fbfc6a31
Paolo Lami <paololami2000@gmail.com>
2021-12-06 22:20:12 +0000

    sensor

--- UltrasonicSensor.py
+++ UltrasonicSensor.py
@@ -44,7 +44,7 @@ try:
    while True:
       dist = distance()
       print ("Measured Distance = %.1f cm" % dist)
-      time.sleep(1)
+      time.sleep(0.01)
  
 # Reset by pressing CTRL + C
 except KeyboardInterrupt:
