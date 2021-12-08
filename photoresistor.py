ae357807b7d556775de43b9deea4cf9600e9180c
Paolo Lami <paololami2000@gmail.com>
2021-12-08 20:21:20 +0000

    loop

--- photoresistor.py
+++ photoresistor.py
@@ -2,5 +2,6 @@ from PCF8591 import PCF8591
 
 sens = PCF8591(0x48)
 
-light = sens.read(1)*10 #reads data from the photoresistor
-print(light)
+while True:
+  light = sens.read(1)*10 #reads data from the photoresistor
+  print(light)
