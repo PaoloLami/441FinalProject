0b4c2da9227f44709f270daa7c14bc404f8de7fb
Paolo Lami <paololami2000@gmail.com>
2021-12-08 20:10:59 +0000

    background fix

--- background.py
+++ background.py
@@ -9,11 +9,6 @@ powerPREV = 0
 angle = 0
 launchCheck = 0
 
-with open('angle.txt', 'w') as f:
-  f.write(str(0)) 
-with open('power.txt', 'w') as f:
-  f.write(str(0)) 
-
 ledPinReset = 19
 ledPinLaunch = 13
 GPIO.setmode(GPIO.BCM)
