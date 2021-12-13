7070ef9b54e9b2820690fcab8e8831b0fd202cb5
Paolo Lami <paololami2000@gmail.com>
2021-12-13 20:56:07 +0000

    light

--- background.py
+++ background.py
@@ -46,7 +46,7 @@ try:
     #Check if user submitted a launch sequence
     elif power != 0 or angle != 0:
       #Check photoresistor to see if balls are in or not
-      if light<1700:
+      if light<1665:
         GPIO.setup(ledPinReset, GPIO.OUT)
         GPIO.output(ledPinReset,1)  
         #Move stepper motor to desired angle
@@ -86,7 +86,7 @@ try:
         print('Resetting')
         stepper.goAngle(90,-1)
         
-      elif light>=1700:
+      elif light>=1665:
         print("No balls remaining, please insert ball")
         GPIO.setup(ledPinReset, GPIO.OUT)
         GPIO.output(ledPinReset,1)
--- photoresistor.py
+++
@@ -1,7 +0,0 @@
-from PCF8591 import PCF8591
-
-sens = PCF8591(0x48)
-
-while True:
-  light = sens.read(1) #reads data from the photoresistor
-  print(light)
